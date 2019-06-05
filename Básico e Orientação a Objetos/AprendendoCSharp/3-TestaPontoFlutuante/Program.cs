7d0e4246d2f9af72314b2ee222ab2e038fc42590
Carlos Cesar Paza <51347040+CarlosPaza@users.noreply.github.com>
2019-06-04 23:51:31 -0300

    Básico CSharp

---
+++ Básico e Orientação a Objetos/AprendendoCSharp/3-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("meu salario é " + salario);
+
+            
+            double divisao = 5.0 / 2;
+            Console.WriteLine(divisao);
+
+            Console.ReadLine();
+        }
+    }
+}
